6bac164c257403199ec77d515a559a3b1135afb1
Ariel Chen <redmochen@gmail.com>
2021-05-19 21:56:02 +0800

    20210519 code review-1

--- main.js
+++ main.js
@@ -1,18 +1,70 @@
 var restaurants = new Array();
+let columnCount = 3;
+
+window.onload = function () {
+    loadJsonFile();
+}
+
+function changeCity() {    
+    var cityDropDownList = document.getElementById("city");
+    city = cityDropDownList.options[cityDropDownList.selectedIndex].value;
+    initialTownDropDownList(city);
+    getContent(city, "city");
+}
+
+function changeTown() {
+    var townDropDownList = document.getElementById("town");
+    town = townDropDownList.options[townDropDownList.selectedIndex].value;
+    getContent(town, "town");
+}
+
+function getContent(item, option) {
+    var index = 0;
+
+    document.getElementById("content").innerHTML = "";
+    switch (option) {
+        case "city":
+            index = getCityContent(item, index);
+            break;
+        case "town":
+            index = getTownContent(item, index);
+            break;
+        default:
+            index = 0;
+            break;
+    }
+    
+    addEmptyBlock("row" + (Math.floor(index / columnCount)), index);
+}
+
+function getCityContent(city, index) {
+    restaurants.forEach(r => {
+        index = optionCompare(index, r.City, city, r)
+    });
+    return index;
+}
+
+function getTownContent(town, index) {
+    restaurants.forEach(r => {
+        index = optionCompare(index, r.Town, town, r)
+    });
+    return index;
+}
 
 function loadJsonFile() {
+
     var requestURL = 'Data.json';
     var xhr = new XMLHttpRequest();
 
     xhr.onload = function () {
-        if (xhr.status == 200) {
+        if (xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
 
             for (var i = 0; i < json.length; i++) {
                 var a = new Restaurant(json[i]);
                 restaurants.push(a);
             }
-            initialCityOptions();
+            initialCityDropDownList();
             initialContent();
 
             document.getElementById("loading").remove();
@@ -25,35 +77,35 @@ function loadJsonFile() {
     return restaurants;
 }
 
-function initialCityOptions() {
+function initialCityDropDownList() {
     var city = document.getElementById("city");
-    var data = new Array();
+    var cityArray = new Array();
 
     restaurants.forEach(element => {
-        data.push(element.City);
+        cityArray.push(element.City);
     });
 
-    const distinctCities = Array.from(new Set(data));
+    const distinctCities = Array.from(new Set(cityArray));
     distinctCities.forEach(element => {
         city.add(new Option(element));
     });
 }
 
-function initialTownOptions(city) {
+function initialTownDropDownList(city) {
     var town = document.getElementById("town");
-    var data = new Array();
+    var townArray = new Array();
 
-    town.length = 0;
+    town.querySelectorAll("option").forEach(town => town.remove());
     town.add(new Option("請選擇鄉鎮區...", "default", true, true));
     town.options[0].disabled = true;
 
     restaurants.forEach(element => {
         if (element.City == city) {
-            data.push(element.Town);
+            townArray.push(element.Town);
         }
     });
 
-    const distinctTowns = Array.from(new Set(data));
+    const distinctTowns = Array.from(new Set(townArray));
     distinctTowns.forEach(element => {
         town.add(new Option(element));
     });
@@ -62,102 +114,77 @@ function initialTownOptions(city) {
 function initialContent() {
     var length = restaurants.length;
     restaurants.forEach((r, index) => {
-        if (index % 3 == 0) {
-            this.addElement("content", "div", "row" + (Math.floor(index / 3)), "row", "");
+        if (index % columnCount == 0) {
+            addElement("content", "div", "row" + (Math.floor(index / columnCount)), "row", "");
         }
-        this.addBlock("row" + (Math.floor(index / 3)), index, r);
+        addBlock("row" + (Math.floor(index / columnCount)), index, r);
     });
-    addEmptyBlock("row" + (Math.floor(length / 3)), length);
-}
-
-function reloadContent(city, town) {
-    var i = 0;
-    document.getElementById("content").innerHTML = "";
-
-    if (city != null) {
-        restaurants.forEach((r) => {
-            i = optionCompare(i, r.City, city, r)
-        });
-    } else if (town != null) {
-        restaurants.forEach((r) => {
-            i = optionCompare(i, r.Town, town, r)
-        });
-    }
-
-    addEmptyBlock("row" + (Math.floor(i / 3)), i);
+    addEmptyBlock("row" + (Math.floor(length / columnCount)), length);
 }
 
 function optionCompare(i, sample, target, r) {
     if (sample == target) {
-        if (i % 3 == 0) {
-            this.addElement("content", "div", "row" + (Math.floor(i / 3)), "row", "");
+        if (i % columnCount == 0) {
+            addElement("content", "div", "row" + (Math.floor(i / columnCount)), "row", "");
         }
-        this.addBlock("row" + (Math.floor(i / 3)), i, r);
+        addBlock("row" + (Math.floor(i / columnCount)), i, r);
         i = ++i;
     }
     return i;
 }
 
 function addBlock(parentRowID, i, r) {
-    this.addElement(parentRowID, "div", "block" + i, "block", "");
-    this.addElement("block" + i, "div", "photo" + i, "block__photo", "");
-    this.addImage("photo" + i, "img", "img" + i, "photo__img", "", r.PicURL);
-    this.addElement("block" + i, "div", "cover" + i, "block__cover", "");
-    this.addElement("block" + i, "div", "city" + i, "block__city", "");
-    this.addElement("city" + i, "p", "city" + i, "city", r.City);
+    addElement(parentRowID, "div", "block" + i, "block", "");
+    addElement("block" + i, "div", "photo" + i, "block__photo", "");
+    addImage("photo" + i, "img", "img" + i, "photo__img", "", r.PicURL);
+    addElement("block" + i, "div", "cover" + i, "block__cover", "");
+    addElement("block" + i, "div", "city" + i, "block__city", "");
+    addElement("city" + i, "p", "city" + i, "city", r.City);
 
-    this.addElement("block" + i, "div", "intro" + i, "block__intro", "");
-    this.addElement("intro" + i, "p", "town" + i, "intro__town", r.Town);
-    this.addElement("intro" + i, "p", "restaurant" + i, "intro__restaurant", r.Name);
-    this.addElement("intro" + i, "div", "dash" + i, "intro__dash", "");
-    this.addElement("intro" + i, "p", "detail" + i, "intro__detail", r.HostWords);
+    addElement("block" + i, "div", "intro" + i, "block__intro", "");
+    addElement("intro" + i, "p", "town" + i, "intro__town", r.Town);
+    addElement("intro" + i, "p", "restaurant" + i, "intro__restaurant", r.Name);
+    addElement("intro" + i, "div", "dash" + i, "intro__dash", "");
+    addElement("intro" + i, "p", "detail" + i, "intro__detail", r.HostWords);
 }
 
 function addEmptyBlock(parentRowID, i) {
-    if (i % 3 == 1) {
+    if (i % columnCount == 1) {
         for (var j = 0; j < 2; j++) {
-            this.addElement(parentRowID, "div", "block" + i, "block block--empty", "");
+            addElement(parentRowID, "div", "block" + ( i + j ), "block block--empty", "");
         }
-    } else if (i % 3 == 2) {
-        this.addElement(parentRowID, "div", "block" + i, "block block--empty", "");
+    } else if (i % columnCount == 2) {
+        addElement(parentRowID, "div", "block" + i, "block block--empty", "");
     }
 }
 
-function addImage(parentID, tagName, id, cls, text, src) {
+function addImage(parentID, tagName, id, elementClass, text, src) {
     var element = document.createElement(tagName);
     var textNode = document.createTextNode(text);
     element.setAttribute("id", id);
-    element.setAttribute("class", cls);
+    element.setAttribute("class", elementClass);
     element.setAttribute("src", src);
     element.appendChild(textNode);
 
     document.getElementById(parentID).appendChild(element);
 }
 
-function addElement(parentID, tagName, id, cls, text) {
+function addElement(parentID, tagName, id, elementClass, text) {
     var element = document.createElement(tagName);
     var textNode = document.createTextNode(text);
     element.setAttribute("id", id);
-    element.setAttribute("class", cls);
+    element.setAttribute("class", elementClass);
     element.appendChild(textNode);
 
     document.getElementById(parentID).appendChild(element);
 }
 
-window.onload = function () {
-    this.loadJsonFile();
+function addBlockElement(parentID, tagName, id, elementClass, text) {
+    var element = document.createElement(tagName);
+    var textNode = document.createTextNode(text);
+    element.setAttribute("id", id);
+    element.setAttribute("class", elementClass);
+    element.appendChild(textNode);
 
-    var citySelect = document.getElementById("city");
-    var townSelect = document.getElementById("town");
-
-    citySelect.onchange = function () {
-        city = citySelect.options[citySelect.selectedIndex].value;
-        initialTownOptions(city);
-        reloadContent(city, null);
-    }
-
-    townSelect.onchange = function () {
-        town = townSelect.options[townSelect.selectedIndex].value;
-        reloadContent(null, town);
-    }
+    document.getElementById(parentID).appendChild(element);
 }
